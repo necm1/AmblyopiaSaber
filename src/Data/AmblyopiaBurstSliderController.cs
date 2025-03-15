448e10381ea048bb36d2e67397523bb5c456ab52
necm1 <necmi.m.hasan@outlook.de>
2025-03-15 05:18:10 +0100

    chore: move all files to src folder

---
+++ src/Data/AmblyopiaBurstSliderController.cs
@@ -0,0 +1,36 @@
+﻿using AmblyopiaSaber.Config;
+using AmblyopiaSaber.Utils;
+using SiraUtil.Objects;
+using UnityEngine;
+using Zenject;
+
+namespace AmblyopiaSaber.Data
+{
+    public class AmblyopiaBurstSliderController : MonoBehaviour, INoteControllerDidInitEvent
+    {
+        private PluginConfig _pluginConfig;
+
+        protected Transform noteCube;
+        private BurstSliderGameNoteController _burstSliderGameNoteController;
+
+        protected GameObject activeNote;
+        protected SiraPrefabContainer container;
+        protected SiraPrefabContainer.Pool activePool;
+
+        [Inject]
+        internal void Init(PluginConfig pluginConfig)
+        {
+            _pluginConfig = pluginConfig;
+            _burstSliderGameNoteController = GetComponent<BurstSliderGameNoteController>();
+
+            _burstSliderGameNoteController.didInitEvent.Add(this);
+
+            noteCube = _burstSliderGameNoteController.gameObject.transform.Find("NoteCube");
+        }
+
+        public void HandleNoteControllerDidInit(NoteControllerBase noteController)
+        {
+            NoteUtils.ApplyConfigToNote(_pluginConfig, noteController, true);
+        }
+    }
+}
---
+++ src/Installers/AmblyopiaSaberCoreInstaller.cs
@@ -0,0 +1,44 @@
+﻿using AmblyopiaSaber.Config;
+using SiraUtil.Interfaces;
+using Zenject;
+using SiraUtil.Objects.Beatmap;
+using SiraUtil.Extras;
+using AmblyopiaSaber.Data;
+
+namespace AmblyopiaSaber.Installers
+{
+    internal class AmblyopiaSaberCoreInstaller : Installer<PluginConfig, AmblyopiaSaberCoreInstaller>
+    {
+        private readonly PluginConfig _pluginConfig;
+
+        public AmblyopiaSaberCoreInstaller(PluginConfig pluginConfig)
+        {
+            _pluginConfig = pluginConfig;
+            if (!_pluginConfig.LoadedYet)
+            {
+                _pluginConfig.LoadedYet = true;
+                _pluginConfig.LeftNoteData.RightEyeVisibility = 0.5f;
+                _pluginConfig.RightNoteData.LeftEyeVisibility = 0.5f;
+            }
+        }
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_pluginConfig).AsSingle();
+            Container.RegisterRedecorator(new BasicNoteRegistration(DecorateNote));
+            Container.RegisterRedecorator(new BurstSliderHeadNoteRegistration(DecorateNote));
+            Container.RegisterRedecorator(new BurstSliderNoteRegistration(DecorateSlider));
+        }
+
+        public GameNoteController DecorateNote(GameNoteController original)
+        {
+            original.gameObject.AddComponent<AmblyopiaController>();
+            return original;
+        }
+
+        public BurstSliderGameNoteController DecorateSlider(BurstSliderGameNoteController original)
+        {
+            original.gameObject.AddComponent<AmblyopiaBurstSliderController>();
+            return original;
+        }
+    }
+}
